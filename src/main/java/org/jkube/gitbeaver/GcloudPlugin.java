0817b6f647fda2e7a13150f6d5938cd0569fbae8
Jan Kneissler <jan.kneissler@breuninger.de>
2022-10-30 00:09:58 +0200

    public constructor

--- src/main/java/org/jkube/gitbeaver/GcloudPlugin.java
+++ src/main/java/org/jkube/gitbeaver/GcloudPlugin.java
@@ -2,11 +2,24 @@ package org.jkube.gitbeaver;
 
 import org.jkube.gitbeaver.plugin.SimplePlugin;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class GcloudPlugin extends SimplePlugin {
 
     public GcloudPlugin() {
         super(
                 GcloudCommand.class
+        );
+    }
+
+    @Override
+    public List<String> getInstallationScript() {
+        return Arrays.asList(
+                "apk add --update python curl  which bash",
+                "curl -sSL -o install.sh https://sdk.cloud.google.com",
+                "bash install.sh"
         );
     }
 
