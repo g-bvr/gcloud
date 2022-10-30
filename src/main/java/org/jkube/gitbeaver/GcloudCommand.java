7924887342f17599be37ba769137f765f91d17bc
Jan Kneissler <jan.kneissler@breuninger.de>
2022-10-30 09:23:37 +0100

    absolute path to binary

--- src/main/java/org/jkube/gitbeaver/GcloudCommand.java
+++ src/main/java/org/jkube/gitbeaver/GcloudCommand.java
@@ -1,12 +1,18 @@
 package org.jkube.gitbeaver;
 
-import org.jkube.gitbeaver.applicationlog.DefaultLogConsole;
 import org.jkube.gitbeaver.util.ExternalProcess;
 
 import java.util.List;
 import java.util.Map;
 
+
 public class GcloudCommand extends AbstractCommand {
+    /**
+     * For security reasons we specify absolute path to binaries (instead of adding their location
+     * in the path environment variablde, so that one cannot temper with which binary is going top be executed)
+     */
+    private static final String GCLOUD_BINARY = "/root/google-cloud-sdk/bin/gcloud";
+
     public GcloudCommand() {
         super(2, null, "gcloud");
     }
@@ -14,7 +20,7 @@ public class GcloudCommand extends AbstractCommand {
     @Override
     public void execute(Map<String, String> variables, WorkSpace workSpace, List<String> arguments) {
         ExternalProcess gcloud = new ExternalProcess();
-        gcloud.command("gcloud", arguments);
+        gcloud.command(GCLOUD_BINARY, arguments);
         gcloud
                 .dir(workSpace.getWorkdir())
                 .successMarker("Cloning into ")
@@ -22,4 +28,5 @@ public class GcloudCommand extends AbstractCommand {
                 .execute();
 
     }
+
 }
